0693f0d25112fc01a5527ce44b79d5aa7b6c0de1
Christophe CLEUET <ch.cleuet@gmail.com>
2018-04-22 14:12:02 +0200

    Bug fix - Hash SHA 256

--- virustotal.py
+++ virustotal.py
@@ -5,15 +5,16 @@ import wget
 def main():
   files = [f for f in os.listdir('.') if os.path.isfile(f)]
   for f in files:
-   print "File : "+ f
-   hash_f=hashlib.sha256(f).hexdigest()
+   print "File :"+ f
+   hash_f=hashlib.sha256(open(f).read()).hexdigest()
+   print "Hash :" + hash_f
    url="https://www.virustotal.com/latest-scan/"+hash_f
    wget.download(url);
    print '\n'
    response=open(hash_f).read()
    if 'Detection ratio:' in response:
     index=response.find('Detection ratio:')
-    print 'Detection ratio : ' + response[index+92:index+100]
+    print 'Detection ratio : ' + response[index+90:index+100]
    else:
     print "The file is not in VirusTotal database."
    print " -----"
